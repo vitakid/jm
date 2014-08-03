c773d5824113102114618556cddaa22a0ef25958
Marten Lienen <marten.lienen@gmail.com>
2014-08-03 23:14:47 +0200

    Specify behavior without a 'self' link

--- spec/jm/dsl/hal_mapper_spec.rb
+++ spec/jm/dsl/hal_mapper_spec.rb
@@ -1,9 +1,9 @@
 describe JM::DSL::HALMapper do
-  context "when mapping a resource with :self link" do
-    let(:person_class) do
-      Struct.new(:first_name, :last_name, :age)
-    end
+  let(:person_class) do
+    Struct.new(:first_name, :last_name, :age)
+  end
 
+  context "when mapping a resource with 'self' link" do
     let(:mapper_class) do
       person = person_class
 
@@ -32,7 +32,9 @@ describe JM::DSL::HALMapper do
 
     context "to a hash" do
       it "should generate a self link" do
-        hash = mapper_class.new.write(person_class.new("Marten", "Lienen", 21))
+        person = person_class.new("Marten", "Lienen", 21)
+
+        hash = mapper_class.new.write(person)
 
         expect(hash).to eq(_links: { self: { href: "/people/marten-lienen" } },
                            age: 21)
@@ -47,6 +49,40 @@ describe JM::DSL::HALMapper do
         person = mapper_class.new.read(hash)
 
         expect(person).to eq(person_class.new("Marten", "Lienen", 21))
+      end
+    end
+  end
+
+  context "when mapping a resource without a 'self' link" do
+    let(:mapper_class) do
+      person = person_class
+
+      Class.new(JM::DSL::HALMapper) do
+        define_method(:initialize) do
+          super(person, Hash)
+        end
+
+        property :age
+      end
+    end
+
+    context "to a hash" do
+      it "should make no difference" do
+        person = person_class.new("Marten", "Lienen", 21)
+
+        hash = mapper_class.new.write(person)
+
+        expect(hash).to eq(age: 21)
+      end
+    end
+
+    context "from a hash" do
+      it "should still instantiate the correct class" do
+        hash = { age: 21 }
+
+        person = mapper_class.new.read(hash)
+
+        expect(person).to eq(person_class.new(nil, nil, 21))
       end
     end
   end
