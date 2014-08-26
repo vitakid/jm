09fc3aaf2ae5c98c2bf8e364715a26f1823d9d05
Marten Lienen <marten.lienen@gmail.com>
2014-08-26 03:17:24 +0200

    Document JM::DSL::HALMapper

--- lib/jm/dsl/hal_mapper.rb
+++ lib/jm/dsl/hal_mapper.rb
@@ -1,6 +1,6 @@
 module JM
   module DSL
-    # Extended DSL for HAL mapping
+    # An extended DSL with specialized methods for mapping to HAL
     class HALMapper < DSL::Mapper
       def initialize(klass)
         instance_mapper = JM::Mappers::InstanceMapper.new(klass, Hash)
@@ -9,10 +9,30 @@ module JM
         super(link_mapper)
       end
 
+      # Map all properties to and from string names
       def property(name, *args, &block)
         super(name.to_s, *args, &block)
       end
 
+      # Configure the mapping to the URI for the "self" link relation
+      #
+      # During {#read} this mapping will be used to instantiate the source
+      # object before applying the pipes to it.
+      #
+      # @example
+      #   self_link "/people/{name}" do
+      #     def read(params)
+      #       Person.new(params["name"])
+      #     end
+      #
+      #     def write(person)
+      #       { name: person.name }
+      #     end
+      #   end
+      # @param [String] uri_template RFC6570 URI template
+      # @param [JM::Mapper] params_mapper Map source object to and from
+      #   template parameters
+      # @param block Define params_mapper inline
       def self_link(uri_template, params_mapper: nil, &block)
         params_mapper = mapper_or_die(params_mapper, &block)
         link_mapper = HAL::LinkMapper.new(uri_template)
@@ -21,6 +41,7 @@ module JM
         @self_link_mapper = mapper
       end
 
+      # @api private
       def self_link_mapper
         @self_link_mapper
       end
@@ -116,6 +137,7 @@ module JM
         pipe(p, **args)
       end
 
+      # @api private
       def accessor_or_die(accessor, &block)
         if block
           block_to_accessor(&block)
@@ -128,6 +150,7 @@ module JM
         end
       end
 
+      # @api private
       def block_to_accessor(&block)
         accessor_class = Class.new(JM::Accessor)
         accessor_class.class_exec(&block)
@@ -135,6 +158,7 @@ module JM
         accessor_class.new
       end
 
+      # @api private
       def mapper_or_die(mapper, &block)
         if block
           block_to_mapper(&block)
@@ -147,6 +171,7 @@ module JM
         end
       end
 
+      # @api private
       def block_to_mapper(&block)
         accessor_class = Class.new(JM::Mapper)
         accessor_class.class_exec(&block)
