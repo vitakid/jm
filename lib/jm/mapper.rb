ba4cf09271a945274ab2a05a9fe9101309bf6862
Marten Lienen <marten.lienen@gmail.com>
2014-08-26 23:49:59 +0200

    Document JM::Mapper

--- lib/jm/mapper.rb
+++ lib/jm/mapper.rb
@@ -1,17 +1,42 @@
 module JM
-  # A {Mapper} reads data from an object and writes it back
+  # A mapper is an abstraction of the conversion of values
   #
-  # Mathematically speaking a {Mapper} is a bijection.
+  # A mapper can convert objects from some kind of left type/structure to some
+  # kind of right type/structure. What this precisely means, is left to the
+  # subclasses of {Mapper}.
+  #
+  # You can visualize it like this
+  #
+  # ```
+  #      <-- read  ---
+  #     /             \
+  # Left               Right
+  #     \             /
+  #      --- write -->
+  # ```
+  #
+  # @example A date-to-string mapper
+  #   class ISOMapper < JM::Mapper
+  #     def read(string)
+  #       Date.iso8601(string)
+  #     end
+  #
+  #     def write(date)
+  #       date.iso8601
+  #     end
+  #   end
   class Mapper
-    # Read data back in as object
+    # Map an object from right to left
     #
-    # @return [Object] The parsed object representation of data
-    def read(data)
+    # @param [Object] object The right representation of an object
+    # @return [Object] The left representation of object
+    def read(object)
     end
 
-    # Write object out as data
+    # Map an object from left to right
     #
-    # @return [Object] The serialized representation of object
+    # @param [Object] object The left representation of an object
+    # @return [Object] The right representation of object
     def write(object)
     end
   end
