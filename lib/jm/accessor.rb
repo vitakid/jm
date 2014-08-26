f6ac1da11774b6f9e76d365bc7a3d5a74ffaba9a
Marten Lienen <marten.lienen@gmail.com>
2014-08-27 00:14:15 +0200

    Document JM::Accessor

--- lib/jm/accessor.rb
+++ lib/jm/accessor.rb
@@ -1,9 +1,28 @@
 module JM
-  # Insert values into and extract values from objects
+  # An accessor is an abstraction over accessing a data structure
+  #
+  # @example Access the fifth character
+  #   class FithCharacterAccessor < JM::Accessor
+  #     def get(string)
+  #       string[4]
+  #     end
+  #
+  #     def set(string, char)
+  #       string[4] = char
+  #     end
+  #   end
   class Accessor
+    # Read some data from object
+    #
+    # @param [Object] object Object to read from
+    # @return [Object] Read data
     def get(object)
     end
 
+    # Write some data to object
+    #
+    # @param [Object] object Object to write to
+    # @param [Object] value Value to write
     def set(object, value)
     end
   end
